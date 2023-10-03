ae83a6e120c3ee5128fe608e2001026a285f043c
SeherKumsar <seherkumsar99@outlook.com>
2023-10-03 13:32:16 +0300

    My Blog Site: Building a Blog - The Basics

--- blog/views.py
+++ blog/views.py
@@ -4,66 +4,61 @@ from django.shortcuts import render
 
 all_posts = [
     {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Maximilian",
+        "slug": "machine-learning",
+        "image": "machine-learning.png",
+        "author": "Seher",
         "date": date(2023, 9, 28),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "title": "Machine Learning",
+        "excerpt": "Machine Learning: A Brief Introduction",
         "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          Machine learning is a subfield of artificial intelligence that enables computers to automatically perform certain tasks by learning from data. 
+          Thanks to machine learning, computers can quickly and effectively perform tasks that humans cannot do or that take too much time.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          The importance of machine learning is that it offers solutions to many problems we face today and makes our lives easier. 
+          For example, smart assistants developed with machine learning can respond to our voice or written commands and 
+          perform operations such as making an appointment, playing music, reading news. Image processing systems developed with machine learning can perform tasks such as face recognition, 
+          license plate recognition, object recognition and can be used in areas such as security, health and education. 
+          Natural language processing systems developed with machine learning have capabilities such as understanding, translating, summarizing and producing texts, 
+          and can be used in areas such as communication, entertainment and education.
         """
     },
     {
         "slug": "programming-is-fun",
         "image": "coding.jpg",
-        "author": "Maximilian",
+        "author": "Seher",
         "date": date(2024, 3, 10),
-        "title": "Programming Is Great!",
+        "title": "Why I Love Programming?",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          Did you ever spend hours searching that one error in your code? 
+          Yep - that's what happened to me yesterday, but I'm not complaining. 
+          In fact, I love programming and I think it's one of the best skills you can have in this digital age. 
+          Let me tell you why.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          First of all, programming is fun. It's like solving puzzles, but with endless possibilities and creativity. 
+          You can make anything you want with code, from websites to games to apps to robots. 
+          You can also express yourself through code, by choosing your own style, tools and languages. 
+          Programming is a form of art, and you are the artist.
         """
     },
     {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
+        "slug": "algorithms",
+        "image": "algorithm.png",
+        "author": "Seher",
         "date": date(2023, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "title": "What is Algorithm?",
+        "excerpt": "An algorithm is a step-by-step design of a solution to solve a specific problem or achieve a specific goal.",
         "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          What is an algorithm, you ask? Well, an algorithm is a set of instructions or rules that tells a computer how to solve a problem or perform a task. 
+          For example, if you want to sort a list of numbers from smallest to largest, 
+          you can use an algorithm that compares each pair of numbers and swaps them if they are in the wrong order. 
+          This way, you can arrange the numbers in ascending order.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          Algorithms are everywhere in our daily lives. 
+          When you use a search engine like Bing, you are using an algorithm that finds the most relevant web pages for your query. 
+          When you use a GPS navigation system, you are using an algorithm that calculates the shortest route to your destination. 
+          When you play a video game, you are using an algorithm that controls the behavior of the characters and the environment.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
         """
     }
 ]
